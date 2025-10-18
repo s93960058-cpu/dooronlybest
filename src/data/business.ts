ceae3385462ecf8d340f68f50cf27aef5e16d1fd
s93960058-cpu <s93960058@gmail.com>
2025-10-18 23:13:35 +0300

    עדכון מספר טלפון ווואטסאפ

--- src/data/business.ts
+++ src/data/business.ts
@@ -2,8 +2,8 @@ import { BusinessInfo } from '../types';
 
 export const businessInfo: BusinessInfo = {
   address: 'יהודה 6, ערד',
-  phone: '+972583522191',
+  phone: '0505215859',
   email: 'info@onlybest.co.il',
   hours: 'ראשון-חמישי: 09:00-18:00',
-  whatsapp: '+972583522191'
+  whatsapp: '0505215859'
 };
