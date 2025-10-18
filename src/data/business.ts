94ac76fb76333a936dfe456bd90038c22cbe0589
s93960058-cpu <s93960058@gmail.com>
2025-10-18 23:34:44 +0300

    Updated business.ts

--- src/data/business.ts
+++ src/data/business.ts
@@ -2,8 +2,8 @@ import { BusinessInfo } from '../types';
 
 export const businessInfo: BusinessInfo = {
   address: 'יהודה 6, ערד',
-  phone: '0505215859',
+  phone: '+972-505215859',
   email: 'info@onlybest.co.il',
   hours: 'ראשון-חמישי: 09:00-18:00',
-  whatsapp: '0505215859'
+  whatsapp: '+972-505215859'
 };
